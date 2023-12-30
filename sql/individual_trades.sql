e090bc08ab1ff619bf65f8f4331560c7289a0f73
pavel <pshishkin@gmail.com>
2023-12-30 20:26:10 +0000

    initiated prediction project

--- sql/individual_trades.sql
+++ sql/individual_trades.sql
@@ -1,3 +1,12 @@
 select count(*), date_trunc('day', timestamp) from trades group by 2 order by 2 desc;
 
 select * from trades where date_trunc('day', timestamp) = '2023-12-24';
+
+
+select count(*), date_trunc('day', timestamp), mint
+from trades
+where sol_volume > 0
+group by 2, 3
+having count(*) > 5
+order by 2 desc, 1 desc
+;
